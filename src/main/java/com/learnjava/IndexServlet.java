3904eec07b2fabdc66ce43a64600e49c6f3a5785
zhengjian <570470242@qq.com>
2023-03-28 23:09:56 +0800

    cookie

--- src/main/java/com/learnjava/IndexServlet.java
+++ src/main/java/com/learnjava/IndexServlet.java
@@ -2,6 +2,7 @@ package com.learnjava;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,18 +14,40 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 从HttpSession获取当前用户名:
         String user = (String) req.getSession().getAttribute("user");
+        String lang = parseLanguageFromCookie(req);
         resp.setContentType("text/html");
         resp.setCharacterEncoding("UTF-8");
         resp.setHeader("X-Powered-By", "JavaEE Servlet");
         PrintWriter pw = resp.getWriter();
-        pw.write("<h1>Welcome, " + (user != null ? user : "Guest") + "</h1>");
-        if (user == null) {
-            // 未登录，显示登录链接:
-            pw.write("<p><a href=\"/signin\">Sign In</a></p>");
+        System.out.println("when start");
+        if (lang.equals("zh")) {
+            pw.write("<h1>你好, " + (user != null ? user : "Guest") + "</h1>");
+            if (user == null) {
+                pw.write("<p><a href=\"/signin\">登录</a></p>");
+            } else {
+                pw.write("<p><a href=\"/signout\">登出</a></p>");
+            }
         } else {
-            // 已登录，显示登出链接:
-            pw.write("<p><a href=\"/signout\">Sign Out</a></p>");
+            pw.write("<h1>Welcome, " + (user != null ? user : "Guest") + "</h1>");
+            if (user == null) {
+                pw.write("<p><a href=\"/signin\">Sign In</a></p>");
+            } else {
+                pw.write("<p><a href=\"/signout\">Sign Out</a></p>");
+            }
         }
+        pw.write("<p><a href=\"/pref?lang=en\">English</a> | <a href=\"/pref?lang=zh\">中文</a>");
         pw.flush();
+    }
+
+    private String parseLanguageFromCookie(HttpServletRequest req) {
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("lang")) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return "en";
     }
 }
---
+++ src/main/java/com/learnjava/LanguageServlet.java
@@ -0,0 +1,38 @@
+package com.learnjava;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+@WebServlet(urlPatterns = "/pref")
+public class LanguageServlet extends HttpServlet {
+
+    // 模拟一个数据库:
+    private static final Set<String> LANGUAGES = new HashSet<>();
+    static {
+        LANGUAGES.add("en");
+        LANGUAGES.add("zh");
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String lang = req.getParameter("lang");
+        if (LANGUAGES.contains(lang)) {
+            // 创建一个新的Cookie:
+            Cookie cookie = new Cookie("lang", lang);
+            // 该Cookie生效的路径范围:
+            cookie.setPath("/");
+            // 该Cookie有效期:
+            cookie.setMaxAge(8640000); // 8640000秒=100天
+            // 将该Cookie添加到响应:
+            resp.addCookie(cookie);
+        }
+        resp.sendRedirect("/");
+    }
+
+}
